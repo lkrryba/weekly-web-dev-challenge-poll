7688d98bf673292e222560580df3eec13673fc2c
Leanne <leanne.rybintsev@gmail.com>
2021-07-20 15:28:53 +0100

    new week, subs

--- prev_challenges/cat_basket/index.js
+++ prev_challenges/cat_basket/index.js
@@ -1,4 +1,4 @@
-import { renderSubmissions, getSubmissions } from "./submissions.js"
+import { renderSubmissions, getSubmissions } from "/submissions.js"
 
 // All awaits need to be wrapped into async iifes, 
 // because top level await (https://v8.dev/features/top-level-await)
@@ -6,10 +6,10 @@ import { renderSubmissions, getSubmissions } from "./submissions.js"
 // https://caniuse.com/?search=top%20level%20await
 
 (async () => {
-    const submissionsJson = "./submissions.json"
-    console.log('me')
-    const subs = (await getSubmissions(submissionsJson)).energy_meter
-    const images = '/imgs/entries/'
+    const submissionsJson = "../../submissions.json"
+    
+    const subs = (await getSubmissions(submissionsJson)).cat_basket
+    const images = './cat_basket_imgs/'
 
     renderSubmissions(subs, images)
 })()
