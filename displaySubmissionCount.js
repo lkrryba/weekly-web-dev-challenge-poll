0aecacb5dc934a74d28e3e6b65c48252f1a4212d
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-10-01 09:28:38 -0400

    Fix missing text for prev challenge subs count
    This was a simple fix. In `displaySubmissionCount.js`, I've added the
    `submissionText` parameter to `submissionCountDisplay.textContent`
    assignment in line 12.
    
    In `index.html`, I removed the "Submissions so far:" text from the
    `#submission-count-display` element to prevent duplication (line 50).

--- displaySubmissionCount.js
+++ displaySubmissionCount.js
@@ -1,11 +1,13 @@
-import { getSubmissions } from "/submissions.js"
+import { getSubmissions } from '/submissions.js'
 
 export async function displaySubmissionCount(challengeName, submissionText) {
-    const submissionsJson = "/submissions.json"
-    const subs = (await getSubmissions(submissionsJson))[challengeName]
+  const submissionsJson = '/submissions.json'
+  const subs = (await getSubmissions(submissionsJson))[challengeName]
 
-    // display num of subs
-    let submissionCountDisplay = document.getElementById("submission-count-display")
-    let submissionCount = Object.keys(subs).length
-    submissionCountDisplay.textContent += submissionCount
+  // display num of subs
+  let submissionCountDisplay = document.getElementById(
+    'submission-count-display'
+  )
+  let submissionCount = Object.keys(subs).length
+  submissionCountDisplay.textContent += submissionText + submissionCount
 }
