cdfc2516a53557f37e53cdd8816c80efc3dd889d
Leanne <leanne.rybintsev@gmail.com>
2021-10-01 13:39:13 +0100

    fix: prev challenges .path instead of .link

--- prevChallengesList.js
+++ prevChallengesList.js
@@ -1,6 +1,7 @@
 export async function renderPreviousChallenges() {
 	const response = await fetch("./prev_challenges/challenges.json")
     const challenges = await response.json()
+    console.log(challenges)
 
 	const container = document.querySelector(".prev_challenges");
     const unorderedList = document.createElement("ul");
@@ -9,7 +10,7 @@ export async function renderPreviousChallenges() {
         const listItem = document.createElement('li')
         const anchorTag = document.createElement('a')
         anchorTag.textContent =challenge.name
-        anchorTag.setAttribute('href', challenge.link)
+        anchorTag.setAttribute('href', challenge.path)
 
         listItem.appendChild(anchorTag)
         unorderedList.appendChild(listItem)
