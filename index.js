c8ca2b44f181f89e3a60fbf9260616cc3cfcec83
Omeir Fawaz <54888682+omeiirr@users.noreply.github.com>
2021-10-05 00:19:18 +0530

    feat: Add random background color
    On page load, change background to a random color from an array of colors.

--- index.js
+++ index.js
@@ -59,3 +59,15 @@ document.querySelector(".confettiClicks").addEventListener("click", function (e)
 });
 
 /* easter egg end */
+
+/* Random background color on page load */
+let colorOptions = [
+  "#AAD1B6", // Green
+  "#B5CAF9", // Blue
+  "#C7B9FF", // Purple
+  "#F0B9DD", // Pink
+  "#F3BF99", // Orange
+  "#FBF3AB", // Yellow
+];
+let selectedColor = colorOptions[Math.floor(Math.random() * colorOptions.length)];
+document.body.style.background = selectedColor;
