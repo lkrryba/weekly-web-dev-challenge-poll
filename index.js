787fb6c7ab8b66ead123f0b5c176559528b1faf2
Omeir Fawaz <54888682+omeiirr@users.noreply.github.com>
2021-10-05 15:39:58 +0530

    Add gradient color option for random background

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ let colorOptions = [
   "#F0B9DD", // Pink
   "#F3BF99", // Orange
   "#FBF3AB", // Yellow
+  "linear-gradient(to right, #c7b9ff, 50%, #f3bf99)", // Purple --> Orange gradient
 ];
 let selectedColor = colorOptions[Math.floor(Math.random() * colorOptions.length)];
 document.body.style.background = selectedColor;
