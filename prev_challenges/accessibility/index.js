f17324824a17960e943f241ee715e8342d85bacf
Muhammad Raza Khan <muhammadraza9763@gmail.com>
2021-10-07 13:59:14 +0530

    Add gradient background to previous challenges

--- prev_challenges/accessibility/index.js
+++ prev_challenges/accessibility/index.js
@@ -1,6 +1,7 @@
 import { renderSubmissions, getSubmissions } from "/submissions.js"
 import { displaySubmissionCount } from "/displaySubmissionCount.js"
 
+
 // All awaits need to be wrapped into async iifes,
 // because top level await (https://v8.dev/features/top-level-await)
 // isn't supported in safari:
@@ -17,3 +18,16 @@ import { displaySubmissionCount } from "/displaySubmissionCount.js"
     displaySubmissionCount(challengeName, submissionText)
     renderSubmissions(subs, images)
 })()
+
+/* Random background color on page load */
+let colorOptions = [
+    "#AAD1B6", // Green
+    "#B5CAF9", // Blue
+    "#C7B9FF", // Purple
+    "#F0B9DD", // Pink
+    "#F3BF99", // Orange
+    "#FBF3AB", // Yellow
+    "linear-gradient(to right, #c7b9ff, 50%, #f3bf99)", // Purple --> Orange gradient
+  ];
+  let selectedColor = colorOptions[Math.floor(Math.random() * colorOptions.length)];
+  document.body.style.backgroundColor = selectedColor;
--- prev_challenges/cat_basket/index.js
+++ prev_challenges/cat_basket/index.js
@@ -17,3 +17,15 @@ import { displaySubmissionCount } from "/displaySubmissionCount.js"
     displaySubmissionCount(challengeName, submissionText)
     renderSubmissions(subs, images)
 })()
+
+let colorOptions = [
+    "#AAD1B6", // Green
+    "#B5CAF9", // Blue
+    "#C7B9FF", // Purple
+    "#F0B9DD", // Pink
+    "#F3BF99", // Orange
+    "#FBF3AB", // Yellow
+    "linear-gradient(to right, #c7b9ff, 50%, #f3bf99)", // Purple --> Orange gradient
+  ];
+  let selectedColor = colorOptions[Math.floor(Math.random() * colorOptions.length)];
+  document.body.style.backgroundColor = selectedColor;
