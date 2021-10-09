0b167a08ed1bd4889a059a9f3c4e5a6c2029bd7e
Sumit Kumar <titanicsumit@gmail.com>
2021-10-09 22:01:26 +0530

    removed solve latest challenge card from previous challenges

--- submissions.js
+++ submissions.js
@@ -1,46 +1,49 @@
 export function renderSubmissions(submissions, imagePath) {
-    // Loop through submissions array
-    submissions.forEach((submission, index) => {
-        // Create a new submission element with a class of submission
-        const submissionElement = document.createElement('div')
-        submissionElement.classList.add('submission')
+  // Loop through submissions array
+  submissions.forEach((submission, index) => {
+    // Create a new submission element with a class of submission
+    // console.log(submission);
+    const submissionElement = document.createElement("div");
+    submissionElement.classList.add("submission");
 
-        // If it is the winner, add the winner class
-        if (submission.winner) submissionElement.classList.add('winner')
+    // If it is the winner, add the winner class
+    if (submission.winner) {
+      submissionElement.classList.add("winner");
+    }
 
-        // Add elements to submission element
-        // First the anchor tag (link)
-        const link = document.createElement('a')
-        link.href = submission.entryURL
-        link.target = '_blank'
-        link.rel = "noopener"
-        submissionElement.appendChild(link)
+    // Add elements to submission element
+    // First the anchor tag (link)
+    const link = document.createElement("a");
+    link.href = submission.entryURL;
+    link.target = "_blank";
+    link.rel = "noopener";
+    submissionElement.appendChild(link);
 
-        // Then, inside the anchor tag, the thumbnail and name.
-        const img = document.createElement('img')
-        img.src = `${imagePath}${submission.thumbnail}`
-        img.loading = "lazy"
-        img.classList.add('submission-img')
-        link.appendChild(img)
-        const nameParagraph = document.createElement('p')
-        nameParagraph.textContent = submission.name
-        link.appendChild(nameParagraph)
-        const altText = document.createAttribute('alt')
-        altText.textContent = `${submission.name}'s submission`
-        img.setAttributeNode(altText)
+    // Then, inside the anchor tag, the thumbnail and name.
+    const img = document.createElement("img");
+    img.src = `${imagePath}${submission.thumbnail}`;
+    img.loading = "lazy";
+    img.classList.add("submission-img");
+    link.appendChild(img);
+    const nameParagraph = document.createElement("p");
+    nameParagraph.textContent = submission.name;
+    link.appendChild(nameParagraph);
+    const altText = document.createAttribute("alt");
+    altText.textContent = `${submission.name}'s submission`;
+    img.setAttributeNode(altText);
 
-        // When most of the image is prepared, remove the placeholder
-        const placeholder = document.getElementById(`placeholder${index}`)
-        if (placeholder) placeholder.remove()
-        
-        // Inject the submission element into the DOM
-        const takePartDiv = document.querySelector('.take-part')
-        takePartDiv.insertAdjacentElement('afterend', submissionElement)
-    })
+    // When most of the image is prepared, remove the placeholder
+    const placeholder = document.getElementById(`placeholder${index}`);
+    if (placeholder) placeholder.remove();
+
+    // Inject the submission element into the DOM
+    const takePartDiv = document.querySelector(".take-part");
+    takePartDiv.insertAdjacentElement("beforebegin", submissionElement);
+  });
 }
 
 export async function getSubmissions(jsonPath) {
-    const response = await fetch(jsonPath)
-    let data = await response.json()
-    return data
+  const response = await fetch(jsonPath);
+  let data = await response.json();
+  return data;
 }
