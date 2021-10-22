6ed1c0d51995e6c0f5e4c40a42068da240321e6c
MichaelLarocca <michaellarocca@live.com>
2021-10-21 21:41:20 -0400

    resolves #55
    This pull request resolves issue number 55, adding icons to the website page footers.

--- components/footer.js
+++ components/footer.js
@@ -22,16 +22,16 @@ connectedCallback(){
   </div>
     <footer>
     <div class="footer-col-one">  
-      <img class="footer-logo" src="../../../imgs/scrimba-black.svg" alt="scrimba-logo">           
-      <a rel="noopener" href="https://scrimba.com/">Scrimba</a>
-      <a rel="noopener" href="https://scrimba.com/learn/weeklychallenge">Weekly Web Dev Challenge</a>
+      <img class="footer-logo" src="../../../imgs/scrimba-black.svg" alt="scrimba-logo">
+      <span class="spacing-span"><svg class="spacing-svg" width="15" height="10" viewBox="0 0 108 72" fill="black" xmlns="http://www.w3.org/2000/svg" class="ei ei-ag "><rect x="48" width="60" height="20" rx="10" class="ei ei-ai "></rect><rect x="25" y="52" width="60" height="20" rx="10" class="ei ei-aj "></rect><rect x="49" y="26" width="36" height="20" rx="10" class="ei ei-ak "></rect><rect y="52" width="20" height="20" rx="10" class="ei ei-al "></rect></svg><a rel="noopener" href="https://scrimba.com/">Scrimba</a></span>
+      <span class="spacing-span"><svg class="spacing-svg" width="15" height="10" viewBox="0 0 108 72" fill="black" xmlns="http://www.w3.org/2000/svg" class="ei ei-ag "><rect x="48" width="60" height="20" rx="10" class="ei ei-ai "></rect><rect x="25" y="52" width="60" height="20" rx="10" class="ei ei-aj "></rect><rect x="49" y="26" width="36" height="20" rx="10" class="ei ei-ak "></rect><rect y="52" width="20" height="20" rx="10" class="ei ei-al "></rect></svg><a rel="noopener" href="https://scrimba.com/learn/weeklychallenge">Weekly Web Dev Challenge</a></span>
     </div>
     <div class="footer-col-two">
       <p>Resources/Social</p>
-      <a rel="noopener" href="https://twitter.com/scrimba">Twitter</a>
-      <a rel="noopener" href="https://www.youtube.com/c/Scrimba">YouTube</a>
-      <a rel="noopener" href="https://discord.gg/xYbN3ukJZY">Discord</a>
-      <a rel="noopener" href="https://scrimba.com/podcast/">Scrimba Podcast</a>
+      <i class="fab fa-twitter"><a rel="noopener" href="https://twitter.com/scrimba">Twitter</a></i>
+      <i class="fab fa-youtube"><a rel="noopener" href="https://www.youtube.com/c/Scrimba">YouTube</a></i>
+      <i class="fab fa-discord"><a rel="noopener" href="https://discord.gg/xYbN3ukJZY">Discord</a></i>
+      <i class="fas fa-podcast"><a rel="noopener" href="https://scrimba.com/podcast/">Scrimba Podcast</a></i>
     </div>
     <div class="footer-col-three">
       <p>Site Engineers</p>
