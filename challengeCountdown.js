70201deb5f45adccb777946992c20114a088a592
Dibas Dauliya <dibasdauliya@gmail.com>
2021-10-27 13:46:56 +0545

    Add link to FAQ page in nav

--- challengeCountdown.js
+++ challengeCountdown.js
@@ -1,25 +1,25 @@
 export const countdown = function (date) {
-    const challengeCountdownEl = document.getElementById(
-        "challenge-countdown-display"
-    );
+  const challengeCountdownEl = document.getElementById(
+    'challenge-countdown-display'
+  )
 
-    const cd = setInterval(function () {
-        const currentDate = new Date();
-        const diffInMiliseconds = date - currentDate;
-        console.log(date)
-        const diffInSeconds = Math.trunc(diffInMiliseconds / 1000);
-        const daysLeft = Math.trunc(diffInSeconds / 3600 / 24);
-        const hoursLeft = Math.trunc(diffInSeconds / 3600) % 24;
-        const minutesLeft = Math.trunc(diffInSeconds / 60) % 60;
-        const secondsLeft = Math.trunc(diffInSeconds) % 60;
+  const cd = setInterval(function () {
+    const currentDate = new Date()
+    const diffInMiliseconds = date - currentDate
+    // console.log(date)
+    const diffInSeconds = Math.trunc(diffInMiliseconds / 1000)
+    const daysLeft = Math.trunc(diffInSeconds / 3600 / 24)
+    const hoursLeft = Math.trunc(diffInSeconds / 3600) % 24
+    const minutesLeft = Math.trunc(diffInSeconds / 60) % 60
+    const secondsLeft = Math.trunc(diffInSeconds) % 60
 
-        const finalString = `${daysLeft}d ${hoursLeft}h ${minutesLeft}m ${secondsLeft}s`;
+    const finalString = `${daysLeft}d ${hoursLeft}h ${minutesLeft}m ${secondsLeft}s`
 
-        challengeCountdownEl.innerHTML = finalString;
+    challengeCountdownEl.innerHTML = finalString
 
-        if (diffInMiliseconds < 0) {
-            challengeCountdownEl.innerHTML = "ENDED";
-            clearInterval(cd);
-        }
-    }, 1000);
-};
+    if (diffInMiliseconds < 0) {
+      challengeCountdownEl.innerHTML = 'ENDED'
+      clearInterval(cd)
+    }
+  }, 1000)
+}
--- prevChallengesList.js
+++ prevChallengesList.js
@@ -1,20 +1,20 @@
 export async function renderPreviousChallenges() {
-	const response = await fetch("./prev_challenges/challenges.json")
-    const challenges = await response.json()
-    console.log(challenges)
+  const response = await fetch('./prev_challenges/challenges.json')
+  const challenges = await response.json()
+  // console.log(challenges)
 
-	const container = document.querySelector(".prev_challenges");
-    const unorderedList = document.createElement("ul");
+  const container = document.querySelector('.prev_challenges')
+  const unorderedList = document.createElement('ul')
 
-    challenges.challenges.forEach((challenge) => {
-        const listItem = document.createElement('li')
-        const anchorTag = document.createElement('a')
-        anchorTag.textContent =challenge.name
-        anchorTag.setAttribute('href', challenge.path)
+  challenges.challenges.forEach((challenge) => {
+    const listItem = document.createElement('li')
+    const anchorTag = document.createElement('a')
+    anchorTag.textContent = challenge.name
+    anchorTag.setAttribute('href', challenge.path)
 
-        listItem.appendChild(anchorTag)
-        unorderedList.appendChild(listItem)
-    })
+    listItem.appendChild(anchorTag)
+    unorderedList.appendChild(listItem)
+  })
 
-    container.insertAdjacentElement('beforeend', unorderedList)
+  container.insertAdjacentElement('beforeend', unorderedList)
 }
