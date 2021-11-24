b37825177de5a600e1fea84374c59df260dba6ed
santhitak <santhita.sk@gmail.com>
2021-11-24 12:38:03 +0700

    refactor: fix path to scripts

--- prev_challenges/name_beautifier/name_beautifier_imgs/index.js
+++ prev_challenges/name_beautifier/name_beautifier_imgs/index.js
@@ -1,13 +1,13 @@
-import { renderSubmissions, getSubmissions } from "/submissions.js"
+import { renderSubmissions, getSubmissions } from "../../../utils/submissions"
 
-// All awaits need to be wrapped into async iifes, 
+// All awaits need to be wrapped into async iifes,
 // because top level await (https://v8.dev/features/top-level-await)
 // isn't supported in safari:
 // https://caniuse.com/?search=top%20level%20await
 
 (async () => {
     const submissionsJson = "../../submissions.json"
-    
+
     const subs = (await getSubmissions(submissionsJson)).energy_meter
     const images = './energy_meter_imgs/'
 
