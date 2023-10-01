7b454a65649b02d9dd1a652e5d8aa1aea6e2b3bd
Vedant Ahire <ahirevedant05@gmail.com>
2023-10-02 01:54:29 +0530

    🚀 I updated the old navbar into its newer version. Fixed the positions of navbar elements, add some fonts in it and add a logo for FAQ which looks nice ✨

--- components/nav.js
+++ components/nav.js
@@ -28,22 +28,32 @@ class Nav extends HTMLElement {
         <div class="nav-icon-line-3"></div>
     </div>
     <nav>
-      <a class="nav-scrimba" href="https://scrimba.com/" rel="noopener" target="_blank"><img class="nav-logo" src="../../../imgs/scrimba-black.svg" alt="scrimba-logo"></a>
+    
       <div class="nav-other-links">
-        ${
-          lastSegment.length !== 0
-            ? `<a href="/" rel="noopener" target="_blank">Home</a>`
-            : ``
-        } 
-       
-        <a href="/hall-of-fame.html" rel="noopener" target="_blank">Hall of Fame</a>
-        <a href="/#prev_challenges">Previous Challenges</a>
-        <a href="/javascriptmas/javascriptmas.html" rel="noopener" target="_blank">JavaScriptmas</a>
-        <a href="/faq.html" rel="noopener" target="_blank">FAQ</a>
-      </div>
+      ${
+        lastSegment.length !== 0
+          ? `<a href="/" class="nav-links" rel="noopener" target="_blank">Home</a>`
+          : ``
+      } 
+        <a class="nav-links" href="/hall-of-fame.html" rel="noopener" target="_blank">Hall of Fame</a>
+        <a class="nav-scrimba nav-links" href="https://scrimba.com/" rel="noopener" target="_blank"><img class="nav-logo" src="../../../imgs/scrimba-black.svg" alt="scrimba-logo"></a>
+        <a class="nav-links" href="/#prev_challenges">Previous Challenges</a>
+        <a class="nav-links" href="/javascriptmas/javascriptmas.html" rel="noopener" target="_blank">JavaScriptmas</a>
+        </div>
+        <a class="nav-links FAQ" href="/faq.html" rel="noopener" target="_blank"><img class="faq-logo" src="../imgs/faq logo.png"></a>
     </nav>    
     `
+
+    // this.innerHTML = `
+    //   <nav>
+
+    //   </nav>
+    // `
+    
   }
 }
 
 customElements.define('nav-component', Nav)
+
+
+
